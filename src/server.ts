821ea5f50fffa890290045d537d8e097dc396507
sachin sharma <sachin.wiz91@gmail.com>
2025-08-04 23:16:50 +0530

    chore: move dotenv config to server.ts and connect MongoDB

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,12 @@
 import express, {type Request, type Response} from "express";
+import dotenv from 'dotenv';
+import connectDB from "./config/db";
+
+dotenv.config();
 
 const app = express();
+
+connectDB();
 
 app.get("/", (req: Request, res: Response) => {
     res.send("server is running fine")
