e065dff81515c07021c3846a833da52d73a57aed
sachin sharma <sachin.wiz91@gmail.com>
2025-08-04 16:13:08 +0530

    chore: add .env and .env.example, update index.ts to use env variables

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,8 @@
 import app from "./server";
+import dotenv from 'dotenv';
 
-app.listen(3000, () => {
-    console.log("Server is running on port: 3000")
-})
+dotenv.config();
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => console.log(`Server running on port: ${PORT}`))
